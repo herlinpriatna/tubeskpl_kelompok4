cb064d5bc59fa36a08fff06ac5e983b912e901b4
muhammadzulfadly <121471995+muhammadzulfadly@users.noreply.github.com>
2023-05-14 20:42:54 +0700

    Menambahkan fitur Pencarian

---
+++ Pencarian/Pencarian/Program.cs
@@ -0,0 +1,38 @@
+﻿namespace Produk
+{
+    public class ProdukList
+    {
+        private List<Class1> daftarProduk;
+
+        public ProdukList()
+        {
+            daftarProduk = new List<Class1>();
+        }
+
+        public void TambahProduk(Class1 produk)
+        {
+            daftarProduk.Add(produk);
+        }
+
+        public Class1 CariProduk(string nama)
+        {
+            return daftarProduk.Find(p => p.Nama == nama);
+        }
+    }
+
+    public class Class1
+    {
+        public string Nama { get; set; }
+        public int Harga { get; set; }
+        public int Stok { get; set; }
+        public int Jumlah { get; set; }
+
+        public Class1(string nama, int harga, int stok)
+        {
+            Nama = nama;
+            Harga = harga;
+            Stok = stok;
+            Jumlah = 1;
+        }
+    }
+}
---
+++ Pencarian/PencarianProduk/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using Produk;
+
+namespace Pencarian
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ProdukList produkList = new ProdukList();
+
+            produkList.TambahProduk(new Class1("Nasi Goreng", 15000, 10));
+            produkList.TambahProduk(new Class1("Mie Ayam", 12000, 5));
+            produkList.TambahProduk(new Class1("Sate Ayam", 25000, 8));
+            produkList.TambahProduk(new Class1("Bakso Sapi", 10000, 12));
+
+            string input;
+            do
+            {
+                Console.Write("Masukkan nama makanan yang ingin dicari: ");
+                input = Console.ReadLine();
+
+                Class1 makanan = produkList.CariProduk(input);
+
+                if (makanan != null)
+                {
+                    Console.WriteLine($"  {makanan.Nama}");
+                    Console.WriteLine($"  Harga: {makanan.Harga}");
+                    Console.WriteLine($"  Jumlah produk: {makanan.Stok}");
+                }
+                else
+                {
+                    Console.WriteLine($"\nMakanan yang Anda cari tidak ditemukan.");
+                }
+
+                Console.Write("\nApakah Anda ingin mencari makanan lain? (y/n) ");
+                input = Console.ReadLine().ToLower();
+                Console.WriteLine("");
+            } while (input == "y");
+        }
+    }
+}
